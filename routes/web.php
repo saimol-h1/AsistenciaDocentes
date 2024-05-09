cbffbe701b7b8661c3141719bbefa6ecb6046971
Saimol <83297212+saimol-h1@users.noreply.github.com>
2024-05-09 14:11:58 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,26 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return "Hola bienvenido a posts";
 });
+
+//#################rutas con parametros#################
+//****************un parametro**********
+
+Route::get('/posts/{post}', function ($post) {
+    return "Hola bienvenido a ${post}";
+});
+
+//*******************dos parametros **********************
+// Route::get('/posts/{post}/{categoria}', function ($post, $categoria) {
+//     return "Hola bienvenido a ${post} de la categoria ${categoria}";
+// });
+
+//****************Parametros variables********************
+// Route::get('/posts/{post}/{categoria?}', function ($post, $categoria = null) {
+//     if ($categoria) {
+//         return "Hola bienvenido a ${post} de la categoria ${categoria}";
+//     }
+//     return "Hola bienvenido a ${post}";
+// });
+
+// Es imporante definir la ruta de manera ordenada, primero los de mayor importancias
+//y los ultimos los menos necesarios ingresar
